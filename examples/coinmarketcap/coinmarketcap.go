8bdc8ecebf31d23399462776e00e89a337e98c29
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2022-04-17 19:28:13 +0300

    csv

--- examples/coinmarketcap/coinmarketcap.go
+++ examples/coinmarketcap/coinmarketcap.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/PuerkitoBio/goquery"
 	"github.com/geziyor/geziyor"
 	"github.com/geziyor/geziyor/client"
@@ -8,17 +11,20 @@ import (
 )
 
 func main() {
+	os.WriteFile("out.csv", []byte{}, 600)
 	geziyor.NewGeziyor(&geziyor.Options{
 		StartURLs: []string{"https://coinmarketcap.com/all/views/all/"},
 		ParseFunc: coinsParse,
-		Exporters: []export.Exporter{&export.JSON{}},
+		Exporters: []export.Exporter{&export.CSV{}},
 	}).Start()
 }
 
 func coinsParse(g *geziyor.Geziyor, r *client.Response) {
-	r.HTMLDoc.Find("tr.cmc-table-row").Each(func(i int, s *goquery.Selection) {
+	coins := r.HTMLDoc.Find("tr.cmc-table-row")
+	coins.Each(func(i int, s *goquery.Selection) {
 		g.Exports <- map[string]interface{}{
-			"name": s.Find("a.cmc-table__column-name--name").Text(),
+			"name":  fmt.Sprintf(s.Find("a.cmc-table__column-name--name").Text()),
+			"price": fmt.Sprintf("$%v", s.Find("a.cmc-link").Text()),
 		}
 	})
 }
