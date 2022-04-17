7465f40cbe66cb62344dadd4be5090a17ca0780f
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2022-04-17 12:23:13 +0300

    Colly basic parsing

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly/v2"
+)
 
 func main() {
-	fmt.Println("SEO")
+	c := colly.NewCollector(
+		colly.AllowedDomains("en.wikipedia.org"),
+	)
+
+	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
+		links := e.ChildAttrs("a", "href")
+		for _, l := range links {
+
+			fmt.Printf("%s\n", l)
+		}
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Visiting: %v\n", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Printf("Processed: %v\n", r.Request.URL)
+	})
+
+	c.Visit("https://en.wikipedia.org/wiki/Web_scraping")
 }
