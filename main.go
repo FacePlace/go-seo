fac7b6e8f32d57fddd012405273fbb0f86c2faf7
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2022-04-20 18:47:27 +0300

    Basic SEO scraping with Colly

--- main.go
+++ main.go
@@ -4,21 +4,72 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/geziyor/geziyor"
-	"github.com/geziyor/geziyor/client"
+	"github.com/gocolly/colly/v2"
 )
 
+type meta struct {
+	property string
+	content  string
+}
+
+type webPage struct {
+	title    string
+	url      string
+	metaTags []meta
+}
+
+func (w *webPage) format() string {
+	str := fmt.Sprintf("")
+
+	str += fmt.Sprintf("TITLE: %v\n", w.title)
+	str += fmt.Sprintln("---")
+	str += fmt.Sprintf("URL: %v\n", w.url)
+	str += fmt.Sprintln("---")
+
+	for _, v := range w.metaTags {
+		str += fmt.Sprintf("%v - %v\n", v.property, v.content)
+		str += fmt.Sprintln("---")
+	}
+
+	return str
+}
+
 func main() {
-	start := time.Now()
+	bench := time.Now()
 
-	// os.WriteFile("out.csv", []byte{}, 600)
-	geziyor.NewGeziyor(&geziyor.Options{
-		StartURLs: []string{"https://finance.yahoo.com/world-indices"},
-		ParseFunc: func(g *geziyor.Geziyor, r *client.Response) {
+	c := colly.NewCollector()
 
-		},
-		// Exporters: []export.Exporter{&export.CSV{}},
-	}).Start()
+	metaTags := []meta{}
+	var title string
+	url := "https://finance.yahoo.com/world-indices"
 
-	fmt.Printf("Finished proccessing: %v", time.Since(start))
+	c.OnHTML("head", func(e *colly.HTMLElement) {
+		title = e.ChildText("title")
+		e.ForEach("meta", func(i int, e *colly.HTMLElement) {
+			property := e.Attr("property")
+			if len(property) == 0 {
+				property = e.Attr("name")
+			}
+			content := e.Attr("content")
+
+			if len(property) > 0 && len(content) > 0 {
+				metaTags = append(metaTags, meta{
+					property,
+					content,
+				})
+			}
+		})
+	})
+
+	c.Visit(url)
+
+	crawledPage := webPage{
+		title,
+		url,
+		metaTags,
+	}
+
+	fmt.Println(crawledPage.format())
+
+	fmt.Printf("Finished proccessing: %v", time.Since(bench))
 }
