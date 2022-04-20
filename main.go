869fd7f4190468161b33d20987ef1309e7798ca6
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2022-04-20 19:01:42 +0300

    Type structuring

--- main.go
+++ main.go
@@ -7,33 +7,6 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-type meta struct {
-	property string
-	content  string
-}
-
-type webPage struct {
-	title    string
-	url      string
-	metaTags []meta
-}
-
-func (w *webPage) format() string {
-	str := fmt.Sprintf("")
-
-	str += fmt.Sprintf("TITLE: %v\n", w.title)
-	str += fmt.Sprintln("---")
-	str += fmt.Sprintf("URL: %v\n", w.url)
-	str += fmt.Sprintln("---")
-
-	for _, v := range w.metaTags {
-		str += fmt.Sprintf("%v - %v\n", v.property, v.content)
-		str += fmt.Sprintln("---")
-	}
-
-	return str
-}
-
 func main() {
 	bench := time.Now()
 
