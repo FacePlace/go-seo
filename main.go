0f338040b691f8619d2ad13d50d5561ee29fc4ee
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2022-04-20 20:59:54 +0300

    Basic concurrency

--- main.go
+++ main.go
@@ -2,26 +2,39 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var urls = []string{
+	"https://finance.yahoo.com/world-indices",
+	"https://www.cnn.com/",
+	"https://www.nytimes.com/",
+	"https://nypost.com/",
+	"https://www.washingtonpost.com/",
+}
+
 func main() {
 	bench := time.Now()
+	var wg sync.WaitGroup
+	wg.Add(len(urls))
 
-	urls := []string{
-		"https://finance.yahoo.com/world-indices",
-		"https://www.cnn.com/",
-		"https://www.nytimes.com/",
-		"https://nypost.com/",
-		"https://www.washingtonpost.com/",
+	proccessed := new(uint8)
+	for i := 0; i < len(urls); i++ {
+		go func(i int) {
+			parseSEO(proccessed, urls[i])
+			wg.Done()
+		}(i)
 	}
 
-	for i, v := range urls {
-		crawledPage := parseMeta(v)
-
-		fmt.Println(crawledPage.format())
-		fmt.Printf("PROCCESSED %v/%v\n", i+1, len(urls))
-	}
-
+	wg.Wait()
 	fmt.Printf("Finished proccessing: %v", time.Since(bench))
+}
+
+func parseSEO(i *uint8, url string) {
+	crawledPage := parseMeta(url)
+
+	fmt.Println(crawledPage.format())
+	*i += 1
+	fmt.Printf("PROCCESSED %v/%v\n", *i, len(urls))
 }
