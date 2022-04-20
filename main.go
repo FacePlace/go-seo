9a4e1aad5d14596d850e690f7f5d24413bc67db2
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2022-04-20 21:10:23 +0300

    Concurrency optimization

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ var urls = []string{
 	"https://www.cnn.com/",
 	"https://www.nytimes.com/",
 	"https://nypost.com/",
-	"https://www.washingtonpost.com/",
+	"https://www.wsj.com/",
+	"https://www.reuters.com/",
+	"https://www.cbsnews.com/",
+	"https://www.cnbc.com/world/",
+	"https://www.npr.org/",
+	// "https://www.washingtonpost.com/",
 }
 
 func main() {
@@ -19,22 +24,22 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(len(urls))
 
-	proccessed := new(uint8)
-	for i := 0; i < len(urls); i++ {
-		go func(i int) {
-			parseSEO(proccessed, urls[i])
+	proccessed := 0
+	for i, url := range urls {
+		go func(i int, url string) {
+			parseSEO(url)
+			proccessed += 1
+			fmt.Printf("PROCCESSED %v/%v\n", proccessed, len(urls))
 			wg.Done()
-		}(i)
+		}(i, url)
 	}
 
 	wg.Wait()
 	fmt.Printf("Finished proccessing: %v", time.Since(bench))
 }
 
-func parseSEO(i *uint8, url string) {
+func parseSEO(url string) {
 	crawledPage := parseMeta(url)
 
 	fmt.Println(crawledPage.format())
-	*i += 1
-	fmt.Printf("PROCCESSED %v/%v\n", *i, len(urls))
 }
